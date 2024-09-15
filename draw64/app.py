ff650a8fdedfa80d1c0b438abaea5d55d27dd721
Guillaume Pratte <guillaume@guillaumepratte.net>
2024-09-15 00:51:36 -0400

    Implement websockets

--- draw64/app.py
+++ draw64/app.py
@@ -1,15 +1,30 @@
-from typing import Annotated
+import asyncio
+import logging
+from typing import Annotated, cast
 
-from fastapi import Depends, FastAPI, HTTPException, Path, Response, status
+from fastapi import (
+    Depends,
+    FastAPI,
+    HTTPException,
+    Path,
+    Response,
+    status,
+    WebSocket,
+    WebSocketDisconnect,
+)
+from fastapi.responses import HTMLResponse
 
 from draw64.connection_manager import WSConnectionManager
 from draw64.image import Image, ImageData
 from draw64.image_collection import ImageCollection, ImageIDAlreadyExistsException
 from draw64.update_image_request import UpdateImageRequest
+from draw64.pubsub import PubSub
 
 app = FastAPI()
 conn_mananger = WSConnectionManager()
 collection = ImageCollection()
+pubsub = PubSub()
+
 
 ImageID = Annotated[str, Path(max_length=30, pattern=r"^[a-zA-Z\d]+$")]
 
@@ -23,6 +38,45 @@ async def validate_image_id(image_id: ImageID):
 
 
 ValidatedImageID = Annotated[str, Depends(validate_image_id)]
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws/img1");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
 
 
 @app.get("/images")
@@ -76,9 +130,14 @@ async def get_image_data(image_id: ValidatedImageID) -> ImageData:
     return collection[image_id].data
 
 
+def _update_image(image_id: ValidatedImageID, update_request: UpdateImageRequest):
+    collection[image_id].update(update_request.command)
+    pubsub.broadcast(image_id, update_request)
+
+
 @app.put("/images/{image_id}")
 async def update_image(image_id: ValidatedImageID, update_request: UpdateImageRequest):
-    collection[image_id].update(update_request.command)
+    _update_image(image_id, update_request)
 
 
 @app.delete(
@@ -89,15 +148,37 @@ async def delete_image(image_id: ValidatedImageID):
     del collection[image_id]
 
 
-# @app.websocket("/ws/{image_id}")
-# async def websocket_endpoint(image_id: ImageID, websocket: WebSocket):
-#     client_id = hash(websocket)
-#     await conn_mananger.connect(websocket)
-#     try:
-#         while True:
-#             data = await websocket.receive_text()
-#             await conn_mananger.send_personal_message(f"You wrote: {data}", websocket)
-#             await conn_mananger.broadcast(f"Client #{client_id} says: {data}")
-#     except WebSocketDisconnect:
-#         conn_mananger.disconnect(websocket)
-#         await conn_mananger.broadcast(f"Client #{client_id} left the chat")
+@app.websocket("/ws/{image_id}")
+async def websocket_endpoint(image_id: ImageID, websocket: WebSocket):
+    # FIXME: might want to ask the caller to properly create its image
+    if image_id not in collection:
+        collection.create_image(image_id)
+
+    message_queue = pubsub.subscribe(image_id)
+    await conn_mananger.connect(websocket)
+    try:
+        while True:
+            done_tasks, pending_tasks = await asyncio.wait(
+                [
+                    asyncio.create_task(websocket.receive_text()),
+                    asyncio.create_task(message_queue.get()),
+                ],
+                return_when=asyncio.FIRST_COMPLETED,
+            )
+            for done in done_tasks:
+                result = done.result()
+                if isinstance(result, str):
+                    try:
+                        update_request = UpdateImageRequest.model_validate_json(result)
+                        _update_image(image_id, update_request)
+                    except Exception:
+                        logging.exception(f"Error handling websocket input: {result}")
+                else:
+                    broadcasted_request = cast(UpdateImageRequest, result)
+                    await websocket.send_text(broadcasted_request.model_dump_json())
+            for pending in pending_tasks:
+                pending.cancel()
+
+    except WebSocketDisconnect:
+        message_queue.unsubscribe()
+        conn_mananger.disconnect(websocket)
---
+++ draw64/pubsub.py
@@ -0,0 +1,30 @@
+from asyncio import Queue
+from collections import defaultdict
+from typing import Any
+
+
+class PubSub:
+    def __init__(self):
+        self._queues: dict[str, set[SubscribedQueue]] = defaultdict(set)
+
+    def subscribe(self, topic: str):
+        queue = SubscribedQueue(self, topic)
+        self._queues[topic].add(queue)
+        return queue
+
+    def unsubscribe(self, topic: str, queue: "SubscribedQueue"):
+        self._queues[topic].remove(queue)
+
+    def broadcast(self, topic: str, message: Any):
+        for queue in self._queues[topic]:
+            queue.put_nowait(message)
+
+
+class SubscribedQueue(Queue):
+    def __init__(self, pubsub: PubSub, topic: str):
+        super().__init__()
+        self._pubsub = pubsub
+        self._topic = topic
+
+    def unsubscribe(self):
+        self._pubsub.unsubscribe(self._topic, self)
